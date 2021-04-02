044bee351e7400f5378e705543c3d36dad62b0eb
TheLastPenguinbender <79594156+TheLastPenguinbender@users.noreply.github.com>
2021-04-02 20:27:51 +0300

    Create baseuefi.sh

---
+++ baseuefi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=trq" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils alsa-utils pulseaudio openssh rsync
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+useradd -m user
+echo user:password | chpasswd
+
+echo "user ALL=(ALL) ALL" >> /etc/sudoers.d/user
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
